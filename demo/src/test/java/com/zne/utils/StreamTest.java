2e15c510205137c514f84bead93b0c7191099cf9
zelentre <zelentre@qq.com>
2023-10-22 22:01:13 +0800

    Update StreamTest.java

--- demo/src/test/java/com/zne/utils/StreamTest.java
+++ demo/src/test/java/com/zne/utils/StreamTest.java
@@ -11,10 +11,10 @@ import java.util.stream.Stream;
 
 /**
  * @author ZNE
- * @since 2023/10/22
  * @see <a href="https://mp.weixin.qq.com/s/wxMTQkVTXFblzZRXWZ-1mQ">简洁又高效的Stream流</a>
  * @see <a href="https://mp.weixin.qq.com/s/krfbyhDMiLr6H2Q-jqSzTw">必须熟练使用的Stream流操作（1）</a>
  * @see <a href="https://mp.weixin.qq.com/s/hd-CeMM47fyudg11WbUTWQ">必须熟练使用的Stream流操作（2）</a>
+ * @since 2023/10/22
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class StreamTest {
@@ -43,7 +43,7 @@ public class StreamTest {
      * 使用数组创建流
      */
     @Test
-    void test03(){
+    void test03() {
         int[] array = {1, 2, 3, 4, 5};
 
         // 通过Arrays.stream()创建流
@@ -56,20 +56,26 @@ public class StreamTest {
      * 使用Stream.iterate()创建无限流
      */
     @Test
-    void test04(){
+    void test04() {
         Stream<Integer> limit = Stream.iterate(0, n -> n + 2).limit(5);
         limit.forEach(System.out::println);
     }
+
     /**
      * 使用Stream.generate()创建无限流
      */
     @Test
-    void test05(){
+    void test05() {
         Stream<Double> stream = Stream.generate(Math::random);
         stream.forEach(System.out::println);
     }
+    // ----------------实操-------------------------------
+
+    /**
+     * 使用filter操作来筛选出所有年龄大于18岁的人
+     */
     @Test
-    void test06(){
+    void test06() {
         int[] array = {1, 2, 3, 4, 5};
 
         // 通过Arrays.stream()创建流
@@ -77,4 +83,5 @@ public class StreamTest {
         OptionalInt reduce = intStream.reduce((a, b) -> a + b);
         System.out.println(reduce);
     }
+
 }
